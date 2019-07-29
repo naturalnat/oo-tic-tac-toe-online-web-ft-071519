b7cffdb9c36ac2dd897713f6a2dcd888d65c68ea
Natalie Taktachev <ntaktach@gmail.com>
2019-07-29 05:49:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,7 +51,7 @@ class TicTacToe
      input = gets.chomp
      index = input_to_index(input)
 
-    if valid_move?(index) 
+    if valid_move?(index)
       move(index)
       display_board
     else
